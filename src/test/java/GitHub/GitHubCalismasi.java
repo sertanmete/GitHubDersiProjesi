11bc55f00fd8ceea3c8ef9c5678c0994d718ff81
sertanmete <sertanmete@gmail.com>
2022-04-08 22:22:41 +0200

    ikinci gonderim

--- src/test/java/GitHub/GitHubCalismasi.java
+++ src/test/java/GitHub/GitHubCalismasi.java
@@ -1,7 +1,11 @@
 package GitHub;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class GitHubCalismasi {
     public static void main (String[] args){
         System.out.println("basladik");
+        System.out.println("hadi bakalim");
+
     }
 }
