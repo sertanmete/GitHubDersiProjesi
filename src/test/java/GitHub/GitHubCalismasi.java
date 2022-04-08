b8cdc41865854c29d8a4eada44a67b93635f7ff4
sertanmete <102917417+sertanmete@users.noreply.github.com>
2022-04-08 18:55:29 +0200

    Update GitHubCalismasi.java

--- src/test/java/GitHub/GitHubCalismasi.java
+++ src/test/java/GitHub/GitHubCalismasi.java
@@ -3,5 +3,6 @@ package GitHub;
 public class GitHubCalismasi {
     public static void main (String[] args){
         System.out.println("basladik");
+        System.out.println("ekledik");
     }
 }
